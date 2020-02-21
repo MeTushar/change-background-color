6bd8928ede6565b986e91f5976f1eb5ef58183f2
MTushar <tushar.mehrotra@intelluslearning.com>
2020-02-21 19:23:34 +0530

    random color generator functionality added instead of taking it from a list randomly

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 const container = document.querySelector("#container");
 const btn = document.querySelector(".btn");
 
-const backgrounds = ["#ffa500", "#003366", "#8b0000", "#794044", "#088da5", "#ccff00", "#a0db8e"];
+//const backgrounds = ["#ffa500", "#003366", "#8b0000", "#794044", "#088da5", "#ccff00", "#a0db8e"];
 console.log(btn);
 let selectedColor; //Can't use const here because if you are declaring const you have to initialize it
 
+const colorStrings = "0123456789abcdef";
 btn.addEventListener('click', function() {
+    selectedColor = "#";
     console.log(container.style.backgroundColor); 
     //it will return you the backgroundColor if specified as a inline style
-    console.log(backgrounds[Math.floor(Math.random()*backgrounds.length)]);
-    selectedColor = backgrounds[Math.floor(Math.random()*backgrounds.length)];
+    for(let i = 0; i < 6; i++) {
+        selectedColor += colorStrings[Math.floor(Math.random()*16)];
+    }
+    console.log(selectedColor);
     container.style.backgroundColor = selectedColor;
 });
+
 
