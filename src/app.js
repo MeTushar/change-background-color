8e30ff5c595ef6afa6dd777c3fa94f181cae849e
MTushar <tushar.mehrotra@intelluslearning.com>
2020-02-21 18:40:59 +0530

    Added functionality to select a random color from array

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const container = document.querySelector("#container");
 const btn = document.querySelector(".btn");
 
+const backgrounds = ["#ffa500", "#003366", "#8b0000", "#794044", "#088da5", "#ccff00", "#a0db8e"];
 console.log(btn);
+let selectedColor; //Can't use const here because if you are declaring const you have to initialize it
 
 btn.addEventListener('click', function() {
     console.log(container.style.backgroundColor); 
     //it will return you the backgroundColor if specified as a inline style
-    container.style.backgroundColor = "#117f51";
-})
+    console.log(backgrounds[Math.floor(Math.random()*backgrounds.length)]);
+    selectedColor = backgrounds[Math.floor(Math.random()*backgrounds.length)];
+    container.style.backgroundColor = selectedColor;
+});
 
